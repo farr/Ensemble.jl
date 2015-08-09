6d4e23b7bf3ea7f05ab06130d17f354f31acbffc
Will Meierjurgen Farr <wmfarr@gmail.com>
2015-08-08 15:59:05 -1000

    Zero-mean bug fixed in Acor.jl, and added method for acl of ensemble.

--- src/Acor.jl
+++ src/Acor.jl
@@ -1,5 +1,7 @@
 module Acor
 
+""" Returns an estimate of the autocorrelation function of the given
+series.  """
 function acf(xs::Array{Float64, 1})
     n = size(xs, 1)
     N = 1
@@ -7,8 +9,12 @@ function acf(xs::Array{Float64, 1})
         N = N << 1
     end
 
+    mu = mean(xs)
+
     ys = zeros(N)
-    ys[1:n] = xs
+    for i in 1:n
+        ys[i] = xs[i] - mu
+    end
 
     ys_tilde = rfft(ys)
     ac = irfft(abs2(ys_tilde), N)
@@ -16,6 +22,8 @@ function acf(xs::Array{Float64, 1})
     ac[1:n]/ac[1]
 end
 
+""" Returns an estimate of the autocorrelation length of the given
+series, or `Inf` if this is not possible to accurately estimate.  """
 function acl(xs::Array{Float64, 1})
     ac = acf(xs)
 
@@ -31,4 +39,28 @@ function acl(xs::Array{Float64, 1})
     return Inf
 end
 
+""" Returns an array giving the estimated ACL for each parameter,
+treating `xs` as the output of an ensemble MCMC (shape `(ndim,
+nwalkers, nsteps)`).  """
+function acl(xs::Array{Float64, 3})
+    ndim = size(xs, 1)
+    nwalk = size(xs, 2)
+    nstep = size(xs, 3)
+
+    acls = zeros(ndim)
+    means = zeros(nstep)
+
+    for i in 1:ndim
+        for k in 1:nstep
+            means[k] = 0.0
+            for j in 1:nwalk
+                means[k] += xs[i,j,k]
+            end
+            means[k] /= nwalk
+        end
+        acls[i] = acl(means)
+    end
+
+    acls
+end
 end
